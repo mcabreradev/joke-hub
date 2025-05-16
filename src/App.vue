e4117e0ba871438a478cd9d69e72662e8b17b4ce
Miguelangel Cabrera <mcabrera.dev@gmail.com>
2025-05-16 01:44:04 -0300

    feat: add @vercel/analytics dependency and integrate Analytics component in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
 <script setup lang="ts">
-import { provide, ref } from 'vue'
-import { useColorMode } from '@vueuse/core'
-import '@/style.css'
+  import { provide, ref } from 'vue'
+  import { useColorMode } from '@vueuse/core'
+  import { Analytics } from '@vercel/analytics/vue';
+  import '@/style.css'
 
-const colorMode = useColorMode({
-  attribute: 'class',
-  modes: {
-    light: '',
-    dark: 'dark',
+  const colorMode = useColorMode({
+    attribute: 'class',
+    modes: {
+      light: '',
+      dark: 'dark',
+    }
+  })
+
+  const isDarkMode = ref(colorMode.value === 'dark')
+
+  const toggleDarkMode = () => {
+    isDarkMode.value = !isDarkMode.value
+    colorMode.value = isDarkMode.value ? 'dark' : 'light'
   }
-})
 
-const isDarkMode = ref(colorMode.value === 'dark')
-
-const toggleDarkMode = () => {
-  isDarkMode.value = !isDarkMode.value
-  colorMode.value = isDarkMode.value ? 'dark' : 'light'
-}
-
-provide('darkMode', {
-  isDarkMode,
-  toggleDarkMode
-})
+  provide('darkMode', {
+    isDarkMode,
+    toggleDarkMode
+  })
 </script>
 
 <template>
+  <Analytics />
   <div class="min-h-screen">
     <router-view v-slot="{ Component }">
       <transition name="page-fade" mode="out-in">
